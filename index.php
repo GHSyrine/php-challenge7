c1590d034830d298a79a3900a7bf25ac513bc143
GHSyrine <161436600+GHSyrine@users.noreply.github.com>
2024-04-03 22:01:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Syrine" => [
+        "city" =>"SaintGemainEnlaye",
+        "weapon" => "gun"
+    ]
+];
+echo "<pre>";
+var_dump($characters);
+echo "</pre>";
+?>
+
+
